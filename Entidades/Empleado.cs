d8808191fa0f01b4796266de38d1610a3bc0e0b7
Mariela Valentini <marielavalentini@outlook.com>
2022-11-03 17:30:23 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Empleado
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        private static int comision = 12;
+
+        public static int Comision
+        {
+            get { return comision; }
+            set { comision = value; }
+        }
+
+        public Empleado(string pId, string pNombre, string pApellido)
+        {
+            Id = pId;
+            Nombre = pNombre;
+            Apellido = pApellido;
+
+        }
+    }
+
+    
+
+}
---
+++ WindowsPresentacion/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsPresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEmpleados_Click(object sender, EventArgs e)
+        {
+            Empleado empleado1 = new Empleado("A12", "Pedro", "Gomez");
+            MessageBox.Show("Comision: " + Empleado.Comision);
+            Empleado empleado2 = new Empleado("A18", "Maria", "Castanio");
+            Empleado.Comision = 25;
+            MessageBox.Show("Comision: " + Empleado.Comision);
+        }
+    }
+}
